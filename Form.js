d843e4ad44ab4d89f8abb901297a89c5f885e8ae
Vedansh-Garg <71022523+Vedansh-Garg@users.noreply.github.com>
2020-12-02 18:05:15 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,30 @@
+class Form {
+     constructor(){
+         this.input = createInput("Name"),
+         this.button = createButton("Enter"),
+         this.greeting = createElement('h3'),
+         this.title = createElement('h2')
+     }
+    hide(){
+        this.greeting.hide();
+        this.input.hide();
+        this.button.hide();
+    }
+    display(){ 
+    this.title.html("Car Racing Game");
+    this.title.position(500, 0);
+    this.input.position(500, 160);
+    this.button.position(500, 200);
+
+this.button.mousePressed(()=>{
+    this.input.hide();
+    this.button.hide();
+    player.name = this.input.value();
+    playerCount+=1;
+    player.index = playerCount;
+    player.update();
+    player.updateCount(playerCount);
+    this.greeting.html("Hello " + player.name );
+    this.greeting.position(500, 160) });
+    }
+}
